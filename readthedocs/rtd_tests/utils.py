fccac1b4e7dcda37629922f3e19fe6f419f77c16
Manuel Kaufmann <humitos@gmail.com>
2018-06-06 11:04:58 -0300

    Restore the cwd by using a decorator helper

--- readthedocs/rtd_tests/utils.py
+++ readthedocs/rtd_tests/utils.py
@@ -12,6 +12,7 @@ from tempfile import mkdtemp
 from django_dynamic_fixture import new
 from django.contrib.auth.models import User
 
+from readthedocs.doc_builder.base import restoring_chdir
 
 log = logging.getLogger(__name__)
 
@@ -38,6 +39,7 @@ def check_output(command, env=None):
     return output
 
 
+@restoring_chdir
 def make_test_git():
     directory = mkdtemp()
     path = get_readthedocs_app_path()
@@ -101,14 +103,13 @@ def make_test_git():
 
     # Checkout to master branch again
     check_output(['git', 'checkout', 'master'], env=env)
-    chdir(path)
     return directory
 
 
+@restoring_chdir
 def create_git_tag(directory, tag, annotated=False):
     env = environ.copy()
     env['GIT_DIR'] = pjoin(directory, '.git')
-    path = getcwd()
     chdir(directory)
 
     command = ['git', 'tag']
@@ -116,9 +117,9 @@ def create_git_tag(directory, tag, annotated=False):
         command.extend(['-a', '-m', 'Some tag'])
     command.append(tag)
     check_output(command, env=env)
-    chdir(path)
 
 
+@restoring_chdir
 def make_test_hg():
     directory = mkdtemp()
     path = get_readthedocs_app_path()
@@ -130,7 +131,6 @@ def make_test_hg():
     log.info(check_output(['hg', 'init'] + [directory]))
     log.info(check_output(['hg', 'add', '.']))
     log.info(check_output(['hg', 'commit', '-u', hguser, '-m"init"']))
-    chdir(path)
     return directory
 
 
